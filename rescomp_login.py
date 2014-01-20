3c8e8eaabd878681d744e1fe2681934231cad281
Chris Kuehl <chris@techxonline.net>
2014-01-19 20:06:23 -0800

    Add executable which logs in to ResComp

---
+++ rescomp_login.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import calnet_login, random, string, getpass
+from urllib.request import urlopen
+
+TEST_ADDRESS = "http://74.125.239.102/" # Google
+RESCOMP_PREFIX = "https://net-auth-"
+RESCOMP_SUFFIX = ".housing.berkeley.edu/"
+RESCOMP_LOGIN_PATH = "cgi-bin/pub/wireless-auth/rescomp.cgi?mode=calnet"
+
+def get_login_server():
+	"""Returns the ResComp login server in use, or False if authentication
+	isn't required."""
+
+	# cache busting
+	rand = "".join(random.choice(string.ascii_letters) for _ in range(20))
+
+	with urlopen("{}?a={}".format(TEST_ADDRESS, rand)) as res:
+		url = res.geturl()
+
+		if url.startswith(RESCOMP_PREFIX) and url.endswith(RESCOMP_SUFFIX):
+			return url
+
+login_server = get_login_server()
+
+if login_server:
+	print("We appear to be using ResComp, proceeding with login.")
+	print("\tLogin server: {}".format(login_server))
+
+	return_url = login_server + RESCOMP_LOGIN_PATH
+	username = input("Enter your CalNet username: ")
+	password = getpass.getpass()
+
+	try:
+		url = calnet_login.login(return_url, username, password)
+		print(url)
+		print("Authenticated successfully, welcome to the internet.")
+	except calnet_login.LoginError as ex:
+		print("Authentication failed with the following error:")
+		print("\t{}".format(ex))
+	except Exception as ex:
+		print("Authentication failed with unexpected error:")
+		print("\t{}".format(ex))
+else:
+	print("No authentication is necessary.")
